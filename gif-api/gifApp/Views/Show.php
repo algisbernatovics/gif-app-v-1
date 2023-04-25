ba016c11bfc43fc5c861e2c7860d0460f58efd57
algisbernatovics <miskastespams@gmai.com>
2023-04-25 17:16:13 +0300

    Update

--- gif-api/gifApp/Views/Show.php
+++ gif-api/gifApp/Views/Show.php
@@ -1,20 +1,11 @@
 <?php
-
-require 'vendor/autoload.php';
-
-use Dotenv\Dotenv;
-
 $searchPhrase = $_POST['searchPhrase'];
 $resultLimit = $_POST['limit'];
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 
 $app = new gifApp\StartUp($searchPhrase, $resultLimit);
 $urls = $app->getUrls();
-$urls = $app->getUrls();
+
 foreach ($urls as $key => $url) {
     $currentUrl = $url->getUrl();
     echo "<image><img src=$currentUrl></image>";
 }
-
-
--- gif-api/index.php
+++ gif-api/index.php
@@ -1,31 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Giff Search engine.</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-</html>
-
-<html>
-
-<body>
-<form action="/index.php" method="post">
-
-    <div><label for="limit">Result limit 1-50:</label></div>
-    <div><input type="number" id="limit" name="limit" min="1" max="50" value="25"></div>
-
-    <h1><label for="searchPhrase">Search for:</label></h1>
-    <h1><input type="text" id="searchPhrase" name="searchPhrase" value="Cat"><br><br></h1>
-    <h1><input type="submit" value="Search"></h1>
-
-</form>
-
 <?php
 
-include 'main.php';
+require 'vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-?>
-</body>
-</html>
+include 'gifApp/Views/Form.php';
+include 'gifApp/Views/Show.php';
